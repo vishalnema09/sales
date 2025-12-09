914f09bdbe28068b3de814b9c7f06ed2b45ec0d3
vishal nema <vishalnema23@gmail.com>
2025-12-09 15:35:30 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "https://sales-ilg2.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE + "/api",
